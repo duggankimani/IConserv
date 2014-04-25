08a4540b2bdfe18179298146322c4827a2cdfc4a
TomKim2012 <tosh0948@gmail.com>
2014-04-25 17:38:17 +0300

    Add Activtiy Interface

--- src/com/wira/pmgt/client/ui/detailedActivity/CreateActivityView.java
+++ src/com/wira/pmgt/client/ui/detailedActivity/CreateActivityView.java
@@ -1,9 +1,17 @@
 package com.wira.pmgt.client.ui.detailedActivity;
 
-import com.gwtplatform.mvp.client.ViewImpl;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewImpl;
+import com.wira.pmgt.client.ui.component.grid.AggregationGrid;
+import com.wira.pmgt.client.ui.component.grid.ColumnConfig;
+import com.wira.pmgt.shared.model.DataType;
+import com.wira.pmgt.shared.model.Listable;
 import com.wira.pmgt.shared.model.program.IsProgramActivity;
 
 public class CreateActivityView extends ViewImpl implements
@@ -15,10 +23,20 @@ public class CreateActivityView extends ViewImpl implements
 	}
 
 	IsProgramActivity Outcome; //The outcome under which this activity is created
-		
+	@UiField AggregationGrid budgetGrid;
+	@UiField AggregationGrid targetGrid;
+	@UiField AggregationGrid outputGrid;
+	@UiField AggregationGrid indicatorGrid;
+	
 	@Inject
 	public CreateActivityView(final Binder binder) {
 		widget = binder.createAndBindUi(this);
+		
+		createBudgetGrid();
+		createIndicatorGrid();
+		createOutputGrid();
+		createTargetGrid();
+		
 	}
 
 	@Override
@@ -28,13 +46,76 @@ public class CreateActivityView extends ViewImpl implements
 
 	@Override
 	public boolean isValid() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public IsProgramActivity getActivity() {
-		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void createBudgetGrid(){
+		budgetGrid.refresh();
+		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+		
+		ColumnConfig budgetItem = new ColumnConfig("budgetItem", "Budget Item", DataType.STRING);
+		configs.add(budgetItem);
+		
+		ColumnConfig config = new ColumnConfig("amount", "Amount", DataType.DOUBLE);
+		config.setAggregationColumn(true);
+		configs.add(config);
+		
+		budgetGrid.setColumnConfigs(configs);
+		budgetGrid.setAutoNumber(true);
+	}
+	
+	
+	public void createTargetGrid(){
+		targetGrid.refresh();
+		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+
+		ColumnConfig measureItem = new ColumnConfig("measure", "Measure", DataType.STRING);
+		configs.add(measureItem);
+		
+		ColumnConfig unitItem = new ColumnConfig("Units", "Units", DataType.DOUBLE);
+		unitItem.setAggregationColumn(true);
+		configs.add(unitItem);
+		
+		targetGrid.setColumnConfigs(configs);
+		targetGrid.setAutoNumber(true);
+	}
+	
+	public void createOutputGrid(){
+		outputGrid.refresh();
+		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+
+		ColumnConfig config = new ColumnConfig("measure", "Measure", DataType.STRING);
+		configs.add(config);
+		
+		ColumnConfig unitItem = new ColumnConfig("Units", "Units", DataType.DOUBLE);
+		unitItem.setAggregationColumn(true);
+		configs.add(unitItem);
+		
+		outputGrid.setColumnConfigs(configs);
+		outputGrid.setAutoNumber(true);
+	}
+	
+	
+	public void createIndicatorGrid(){
+		indicatorGrid.refresh();
+		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+		
+
+		ColumnConfig config = new ColumnConfig("measure", "Measure", DataType.STRING);
+		configs.add(config);
+		
+		ColumnConfig unitItem = new ColumnConfig("Units", "Units", DataType.DOUBLE);
+		unitItem.setAggregationColumn(true);
+		configs.add(unitItem);
+		
+		indicatorGrid.setColumnConfigs(configs);
+		indicatorGrid.setAutoNumber(true);
+	}
+	
+	
 }
