f885e739422c64deb8bd2d591a58248669eb7676
TomKim2012 <tosh0948@gmail.com>
2014-11-05 16:17:25 +0300

    Added a Refresh mechanism on FormAssign

--- src/com/wira/pmgt/client/ui/assign/FormAssign.java
+++ src/com/wira/pmgt/client/ui/assign/FormAssign.java
@@ -3,6 +3,9 @@ package com.wira.pmgt.client.ui.assign;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Anchor;
@@ -10,6 +13,7 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.wira.pmgt.client.service.TaskServiceCallback;
+import com.wira.pmgt.client.ui.events.ProcessingCompletedEvent;
 import com.wira.pmgt.client.util.AppContext;
 import com.wira.pmgt.shared.model.ProgramDetailType;
 import com.wira.pmgt.shared.model.program.ProgramTaskForm;
@@ -22,7 +26,9 @@ public class FormAssign extends Composite {
 			.create(FormAssignUiBinder.class);
 	
 	@UiField Anchor aCreateForm;
+	@UiField Anchor  aReload;
 	@UiField HTMLPanel divAllocations;
+	@UiField SpanElement spnRefresh;
 	
 	Long programId=null;
 	
@@ -32,6 +38,13 @@ public class FormAssign extends Composite {
 
 	public FormAssign() {
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		aReload.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				loadTaskForms();
+			}
+		});
 	}
 	
 	public void setActivityId(Long activityId, ProgramDetailType type) {
@@ -49,15 +62,22 @@ public class FormAssign extends Composite {
 	@Override
 	protected void onLoad() {
 		super.onLoad();
+		loadTaskForms();
+	}
+	
+	
+	public void loadTaskForms(){
+		spnRefresh.setClassName("icon-spinner icon-spin");
 		if(programId!=null){
-		AppContext.getDispatcher().execute(new GetProgramTaskForm(programId), 
-				new TaskServiceCallback<GetProgramTaskFormResponse>() {
-			@Override
-			public void processResult(
-					GetProgramTaskFormResponse aResponse) {
-				setTaskForms(aResponse.getTaskForms());
+			AppContext.getDispatcher().execute(new GetProgramTaskForm(programId), 
+					new TaskServiceCallback<GetProgramTaskFormResponse>() {
+				@Override
+				public void processResult(
+						GetProgramTaskFormResponse aResponse) {
+					spnRefresh.setClassName("icon-refresh");
+					setTaskForms(aResponse.getTaskForms());
+				}
+			});
 			}
-		});
-		}
 	}
 }
